b749dff7a4dfef6968fb98334748bd860317352a
Slapppy <42652345+Slapppy@users.noreply.github.com>
2022-04-21 03:55:35 +0300

    Меткие изменения в Benchmark

--- SplayTreeIm/benchmark/Benchmark.py
+++ SplayTreeIm/benchmark/Benchmark.py
@@ -1,13 +1,9 @@
 import time
 from csv import writer
 import pandas as pd
-from random import randint
 from SplayTreeIm.SplayTree import SplayTree, Node
 
 spltree = SplayTree()
-n = 10000
-dataset = []
-dataset1 = [i for i in range(0, n)]
 
 
 def test_insert(dataset, tree=spltree):
@@ -15,7 +11,7 @@ def test_insert(dataset, tree=spltree):
     for i in dataset:
         tree.insert(Node(i))
     t2 = time.time()
-    return f"%.8f" % (t2 - t1)
+    return "%.8f" % (t2 - t1)
 
 
 def test_search(dataset, tree=spltree):
@@ -23,7 +19,7 @@ def test_search(dataset, tree=spltree):
     for i in dataset:
         tree.search_tree(Node(i))
     t2 = time.time()
-    return f"%.8f" % (t2 - t1)
+    return "%.8f" % (t2 - t1)
 
 
 def test_delete(dataset, tree=spltree):
@@ -31,7 +27,7 @@ def test_delete(dataset, tree=spltree):
     for i in dataset:
         tree.delete(Node(i))
     t2 = time.time()
-    return f"%.8f" % (t2 - t1)
+    return "%.8f" % (t2 - t1)
 
 
 def worst_case_insert():
@@ -40,9 +36,10 @@ def worst_case_insert():
             _list = []
             for line in file:
                 _list.append(line)
-            with open(r'C:\Users\Slappy\PycharmProjects\SplayTree\SplayTreeIm\benchmark\Results\insert_worst.csv' , 'a') as case:
+            with open('Results/insert_worst.csv', 'a') as case:
                 write_line = writer(case)
-                write_line.writerow(['insert']+[len(_list)] + [test_insert(_list)])
+                write_line.writerow(['insert'] + [len(_list)] + [test_insert(_list)])
+
 
 def worst_case_search():
     for i in range(1, 5 + 1):
@@ -50,9 +47,10 @@ def worst_case_search():
             _list = []
             for line in file:
                 _list.append(line)
-            with open(r'C:\Users\Slappy\PycharmProjects\SplayTree\SplayTreeIm\benchmark\Results\search_worst.csv' , 'a') as case:
+            with open('Results/search_worst.csv', 'a') as case:
                 write_line = writer(case)
-                write_line.writerow(['search']+[[len(_list)]] + [test_search(_list)])
+                write_line.writerow(['search'] + [len(_list)] + [test_search(_list)])
+
 
 def worst_case_delete():
     for i in range(1, 5 + 1):
@@ -60,9 +58,10 @@ def worst_case_delete():
             _list = []
             for line in file:
                 _list.append(line)
-            with open(r'C:\Users\Slappy\PycharmProjects\SplayTree\SplayTreeIm\benchmark\Results\delete_worst.csv' , 'a') as case:
+            with open('Results/delete_worst.csv','a') as case:
                 write_line = writer(case)
-                write_line.writerow(['delete']+[len(_list)] + [test_delete(_list)])
+                write_line.writerow(['delete'] + [len(_list)] + [test_delete(_list)])
+
 
 def case_insert():
     for i in range(1, 5 + 1):
@@ -70,9 +69,11 @@ def case_insert():
             _list = []
             for line in file:
                 _list.append(line)
-            with open(r'C:\Users\Slappy\PycharmProjects\SplayTree\SplayTreeIm\benchmark\Results\insert.csv' , 'a') as case:
+            with open('Results/insert.csv',
+                      'a') as case:
                 write_line = writer(case)
-                write_line.writerow(['insert']+[len(_list)] + [test_insert(_list)])
+                write_line.writerow(['insert'] + [len(_list)] + [test_insert(_list)])
+
 
 def case_search():
     for i in range(1, 5 + 1):
@@ -80,9 +81,11 @@ def case_search():
             _list = []
             for line in file:
                 _list.append(line)
-            with open(r'C:\Users\Slappy\PycharmProjects\SplayTree\SplayTreeIm\benchmark\Results\search.csv' , 'a') as case:
+            with open('Results/search.csv',
+                      'a') as case:
                 write_line = writer(case)
-                write_line.writerow(['search']+[len(_list)] + [test_search(_list)])
+                write_line.writerow(['search'] + [len(_list)] + [test_search(_list)])
+
 
 def case_delete():
     for i in range(1, 5 + 1):
@@ -90,9 +93,11 @@ def case_delete():
             _list = []
             for line in file:
                 _list.append(line)
-            with open(r'C:\Users\Slappy\PycharmProjects\SplayTree\SplayTreeIm\benchmark\Results\delete.csv' , 'a') as case:
+            with open('Results/delete.csv',
+                      'a') as case:
                 write_line = writer(case)
-                write_line.writerow(['delete']+[len(_list)] + [test_delete(_list)])
+                write_line.writerow(['delete'] + [len(_list)] + [test_delete(_list)])
+
 
 worst_case_insert()
 worst_case_search()
